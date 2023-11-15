64e028791f5fe7cd44b3f8b76fed7c1eab413774
mdanishu <mdanishu92@gmail.com>
2023-11-15 02:12:20 -0500

    hopefully final fix of next config

--- next.config.js
+++ next.config.js
@@ -1,15 +1,5 @@
 /** @type {import('next').NextConfig} */
-<<<<<<< HEAD
-const nextConfig = {
-    basePath: '/next',
-    assetPrefix: '/next/'
-  }
-  
-  module.exports = nextConfig
-  
-=======
+
 const nextConfig = {}
 
-
 module.exports = nextConfig
->>>>>>> temp-branch
